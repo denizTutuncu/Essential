8d55c21464fe766e5318dbcf36d304887c53bfcd
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-08-19 23:30:14 -0700

    Save command requests cache deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,16 +6,25 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
+    private let store: FeedStore
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
     var deleteCachedFeedCallCout = 0
     
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCout += 1
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -23,6 +32,27 @@ class CacheFeedUseCaseTests: XCTestCase {
     func test_init_doesNotDeleteCacheUponCreation() {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
+        
         XCTAssertEqual(store.deleteCachedFeedCallCout, 0)
     }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCout, 1)
+    }
+    
+    //MARK:- helpers
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
+    
 }
