57e07b9846ad54244f2de14dd637aa44eaba2a08
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-08-20 15:45:57 -0700

    Move 'FeedStore' to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Deniz Tutuncu on 8/20/21.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
