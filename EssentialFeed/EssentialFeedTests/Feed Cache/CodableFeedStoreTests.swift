9f52bdc41dd29ef0fbc266eedff1907095ebed81
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-08-22 17:44:36 -0700

    Add (forgotten) expect helper method to complete the test

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -92,6 +92,8 @@ class CodableFeedStoreTests: XCTestCase {
         
         let insertionError = insert((feed, timestamp), to: sut)
         XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
+        
+        expect(sut, toRetrieve: .empty)
     }
     
     func test_delete_hasNoSideEffectsOnEmptyCache() {
