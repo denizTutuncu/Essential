5946002f0a9c189ff86daf91d5129059e590052d
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-08-31 15:21:22 -0700

    `CoreDataFeedStore.retrieve()` delivers empty on empty cache

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -10,15 +10,15 @@ import Foundation
 public class CoreDataFeedStore: FeedStore {
     public init() {}
     
-    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         
     }
     
-    public func retrieve(completion: @escaping RetrievalCompletion) {
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
     }
 }
