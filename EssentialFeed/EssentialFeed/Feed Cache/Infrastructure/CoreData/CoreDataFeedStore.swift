0ec48e93e2c8063faad0cea9cc20fe36d9307377
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-08-31 15:18:09 -0700

    `CoreDataFeedStore` conforms to `FeedStore`

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
-public class CoreDataFeedStore {
- 
+public class CoreDataFeedStore: FeedStore {
+    public init() {}
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        
+    }
 }
