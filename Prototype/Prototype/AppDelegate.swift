17a5619891503f2fe9aa65b6bf8aa70dc5351935
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-09-02 14:06:26 -0700

    Add empty project for prototype

---
+++ Prototype/Prototype/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Prototype
+//
+//  Created by Deniz Tutuncu on 9/2/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
