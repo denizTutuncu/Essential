b7a8ba9e2c01450bcb71f9663f2c6f8e7988a9eb
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-09-15 14:34:12 -0700

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Deniz Tutuncu on 9/14/21.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
