eaad69430883d4d1b26f36d1104e860cecbe22a4
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-11-03 14:12:14 -0700

    Move `FeedErrorViewModel` to separate file.

---
+++ EssentialFeed/EssentialFeediOS/Feed Presenttion/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Deniz Tutuncu on 11/3/21.
+//
+
+import Foundation
+
+public struct FeedErrorViewModel {
+    public let message: String?
+}
