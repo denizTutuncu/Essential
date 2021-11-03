be56b9f4df6fcb3a2601d7fd16f476b7c6065e51
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-11-03 16:34:41 -0700

    Move `FeedViewModel` to separate file.

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Deniz Tutuncu on 11/3/21.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
