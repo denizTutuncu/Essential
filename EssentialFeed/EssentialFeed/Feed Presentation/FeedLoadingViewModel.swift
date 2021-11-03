c7892355c95798dbf39d5457e8a26caebcbf3849
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-11-03 16:35:40 -0700

    Move `FeedLoadingViewModel` to separate file.

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Deniz Tutuncu on 11/3/21.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
