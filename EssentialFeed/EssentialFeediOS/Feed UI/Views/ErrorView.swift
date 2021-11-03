ce27ead0dcf4e49e660be27a30a2e470ecaccb0b
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-11-03 13:44:21 -0700

    Move `ErrorView` to separate file.

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Deniz Tutuncu on 11/3/21.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
+
